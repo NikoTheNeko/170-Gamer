39caeecac7de21182652d232f03d2c1bbbd012b3
Nikolas Sanchez <yoshiniko1223@gmail.com>
2020-10-28 00:45:31 -0700

    Organized A bunhc of stuff
    hohoo

---
+++ Assets/Our Game/Prototypes Split 1/Chopping Prototype/Sliceable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sliceable : MonoBehaviour
+{
+    public List<GameObject> pieces;
+    private bool[] sliced;
+    private void Awake()
+    {
+        sliced = new bool[pieces.Count];
+        for (int i = 0; i < sliced.Length; i++)
+        {
+            sliced[i] = false;
+        }
+    }
+
+    private int GetLastPieceIndex()
+    {
+        int i;
+        for (i = -1; i < sliced.Length - 1; i++)
+        {
+            if (sliced[i + 1] == true)
+            {
+                break;
+            }
+        }
+        return i;
+    }
+
+    public Vector2 GetNextSlicePos()
+    {
+        int ind = GetLastPieceIndex();
+        return pieces[ind].transform.position + (Vector3.left * (pieces[ind].GetComponent<SpriteRenderer>().bounds.size.x) / 2);
+    }
+    public bool Slice() //to do, returns whether or not an obj has been sliced fully
+    {
+        int ind = GetLastPieceIndex();
+        pieces[ind].GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(0.2f, 0.8f), Random.Range(-0.3f, 0.3f)), ForceMode2D.Impulse);
+        sliced[ind] = true;
+        Debug.Log("PSDIGJIOSDGJIOSDGIOSEGJOJ");
+        return GetLastPieceIndex() <= 0;
+    }
+}
