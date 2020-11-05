a2625f72c8e8cab6c48a9e0b058ecabb73fc6c70
Ryan Heath <rheath@ucsc.edu>
2020-11-05 14:25:54 -0800

    ahaaa me me fun boy

---
+++ Assets/Our Game/Prototypes Split 1/Pour Prototype/Scripts/PotScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotScript : MonoBehaviour
+{
+    public float speed = 10f;
+
+    bool isRotating = false;
+    Vector2 movementVec;
+    Quaternion startRotation;
+    Vector3 startPos;
+
+    private void Start()
+    {
+        startRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isRotating = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isRotating = false;
+        }
+
+        if (isRotating)
+        {
+            if (transform.up.y >= -0.25)
+            {
+                transform.Rotate(new Vector3(0, 0, 5) * Time.deltaTime * speed);
+            }
+        }
+        else
+        {
+            if(transform.up.y != 1)
+            {
+                transform.Rotate(new Vector3(0, 0, 5) * Time.deltaTime * (-speed));
+            }
+        }
+    }
+}
