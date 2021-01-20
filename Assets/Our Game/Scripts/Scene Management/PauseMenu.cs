573bf931c2335291632510fc93f0c077910ae7c3
YellowSwerve <57231393+YellowSwerve@users.noreply.github.com>
2021-01-20 15:37:57 -0800

    added pause menu to cooking scene
    can now pause with esc and choose what you want to do. Esc no longer just takes you to scene select so essentially the wait go back script is obsolete now.

--- Assets/Our Game/Scripts/Scene Management/PauseMenu.cs
+++ Assets/Our Game/Scripts/Scene Management/PauseMenu.cs
@@ -19,7 +19,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (IsPaused)
             {
